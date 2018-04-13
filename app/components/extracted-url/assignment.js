9031889133d120ee402c85093cfcc1b10277f1b2
Jacob Bare <jacob@limit0.io>
2018-04-13 08:50:40 -0500

    Add customer and tag assignment saving

--- app/components/extracted-url/assignment.js
+++ app/components/extracted-url/assignment.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
-import { computed, set } from '@ember/object';
+import { computed, get } from '@ember/object';
 import ComponentQueryManager from 'ember-apollo-client/mixins/component-query-manager';
+
+import urlCustomerMutation from 'leads-manage/gql/mutations/extracted-url-customer';
+import urlTagsMutation from 'leads-manage/gql/mutations/extracted-url-tags';
+import hostCustomerMutation from 'leads-manage/gql/mutations/extracted-host-customer';
+import hostTagsMutation from 'leads-manage/gql/mutations/extracted-host-tags';
 
 export default Component.extend(ComponentQueryManager, {
   classNames: ['card'],
@@ -10,6 +15,7 @@ export default Component.extend(ComponentQueryManager, {
    * Services
    */
   // loading: service(),
+  notify: inject(),
   errorProcessor: inject(),
 
   /**
@@ -33,25 +39,51 @@ export default Component.extend(ComponentQueryManager, {
     return 0 === value.indexOf('http') ? 'url' : 'host';
   }),
 
+  doSet(mutation, input, resultKey, cb) {
+    const variables = { input };
+    return this.get('apollo').mutate({ mutation, variables }, resultKey);
+  },
+
+  setUrlCustomer(customer) {
+    const resultKey = 'extractedUrlCustomer';
+    const input = { urlId: this.get('model.id'), customerId: get(customer, 'id') };
+    return this.doSet(urlCustomerMutation, input, resultKey);
+  },
+
+  setUrlTags(tags) {
+    const resultKey = 'extractedUrlTags';
+    const input = { urlId: this.get('model.id'), tagIds: tags.map(tag => get(tag, 'id')) };
+    return this.doSet(urlTagsMutation, input, resultKey);
+  },
+
+  setHostCustomer(customer) {
+    const resultKey = 'extractedHostCustomer';
+    const input = { hostId: this.get('model.id'), customerId: get(customer, 'id') };
+    return this.doSet(hostCustomerMutation, input, resultKey);
+  },
+
+  setHostTags(tags) {
+    const resultKey = 'extractedHostTags';
+    const input = { hostId: this.get('model.id'), tagIds: tags.map(tag => get(tag, 'id')) };
+    return this.doSet(hostTagsMutation, input, resultKey);
+  },
+
   actions: {
     setCustomer(customer) {
-      this.set('model.customer', customer);
-      console.info('setCustomer', customer);
-      alert('send setCustomer action for URL');
+      const promise = this.get('scope') === 'url' ? this.setUrlCustomer(customer) : this.setHostCustomer(customer);
+      promise
+        .then(() => this.get('notify').info('Customer successfully assigned.'))
+        .then(() => this.set('model.customer', customer))
+        .catch(e => this.get('errorProcessor').show(e))
+      ;
     },
     setTags(tags) {
-      this.set('model.tags', tags);
-      console.info('setTags', tags);
-      alert('send setTags action for URL');
-    },
-    saveAssignment() {
-      alert('saveAssignment');
-      // const loading = this.get('loading');
-      // loading.show();
-      // this.get('extracted').save()
-      //   .catch(adapterError => this.get('errorProcessor').notify(adapterError.errors))
-      //   .finally(() => loading.hide())
-      // ;
+      const promise = this.get('scope') === 'url' ? this.setUrlTags(tags) : this.setHostTags(tags);
+      promise
+        .then(() => this.get('notify').info('Tags successfully assigned.'))
+        .then(() => this.set('model.tags', tags))
+        .catch(e => this.get('errorProcessor').show(e))
+      ;
     },
   },
 
