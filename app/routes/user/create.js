d5b10996503d0bc97305b9e68bbdeb61caede04f
Jacob Bare <jacob@limit0.io>
2018-04-15 20:23:54 -0500

    Add user create and update

--- app/routes/user/create.js
+++ app/routes/user/create.js
@@ -1,4 +1,34 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import mutation from 'leads-manage/gql/mutations/create-user';
+
+export default Route.extend(RouteQueryManager, {
+  model() {
+    return {};
+  },
+
+  actions: {
+    create({
+      email,
+      givenName,
+      familyName,
+      password,
+      confirmPassword,
+    }) {
+      const payload = {
+        email,
+        givenName,
+        familyName,
+        password,
+        confirmPassword,
+      };
+      const variables = { input: { payload } };
+      return this.get('apollo').mutate({ mutation, variables }, 'createUser')
+        .then(response => this.transitionTo('user.edit', response.id))
+        .then(() => this.get('notify').info('User created successfully.'))
+        .catch(e => this.get('graphErrors').show(e))
+      ;
+    },
+  },
 });
--- app/routes/user/edit.js
+++ app/routes/user/edit.js
@@ -1,4 +1,34 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import query from 'leads-manage/gql/queries/user';
+import deleteUser from 'leads-manage/gql/mutations/delete-user';
+import updateUser from 'leads-manage/gql/mutations/update-user';
+
+export default Route.extend(RouteQueryManager, {
+  model({ id }) {
+    const variables = { input: { id } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'user');
+  },
+  actions: {
+    update(model) {
+      const mutation = updateUser;
+      const { id, email, givenName, familyName } = model;
+      const payload = { email, givenName, familyName };
+      const input = { id, payload };
+      const variables = { input };
+      return this.get('apollo').mutate({ mutation, variables }, 'updateUser')
+        .then(() => this.get('notify').success('User successfully updated.'))
+        .catch(e => this.get('graphErrors').show(e))
+      ;
+    },
+    delete(id, routeName) {
+      const mutation = deleteUser;
+      const variables = { input: { id } };
+      return this.get('apollo').mutate({ mutation, variables }, 'deleteUser')
+        .then(() => this.transitionTo(routeName))
+        .catch(e => this.get('graphErrors').show(e))
+      ;
+    },
+  },
 });
