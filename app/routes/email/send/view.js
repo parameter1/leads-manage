ffd9bbbd3b919fb5b75117a3dbac96a0a6175a9b
Jacob Bare <jacob@limit0.io>
2018-04-22 09:56:39 -0500

    Add email send view with metrics

--- app/routes/email/send/view.js
+++ app/routes/email/send/view.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import query from 'leads-manage/gql/queries/email-send';
+
+export default Route.extend(RouteQueryManager, {
+  model({ id }) {
+    const variables = { input: { id } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'emailSend');
+  },
 });
