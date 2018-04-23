a2aea01893a82f1951c42a951ac37941ed368e3b
Jacob Bare <jacob@limit0.io>
2018-04-23 13:14:19 -0500

    Add email category view/edit

--- app/routes/email/category/view.js
+++ app/routes/email/category/view.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import query from 'leads-manage/gql/queries/email-category';
+
+export default Route.extend(RouteQueryManager, {
+  model({ id }) {
+    const variables = { input: { id } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'emailCategory');
+  },
+
+  actions: {
+    update() {
+
+    },
+  },
 });
