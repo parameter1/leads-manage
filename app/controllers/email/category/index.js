6fb21b91743c4106c12b99090f9d8b7a2739ba9b
Jacob Bare <jacob@limit0.io>
2018-04-23 12:52:16 -0500

    Add email category list items

--- app/controllers/email/category/index.js
+++ app/controllers/email/category/index.js
@@ -1,4 +1,14 @@
-import Controller from '@ember/controller';
+import ListController from '../../abstract-list';
 
-export default Controller.extend({
+export default ListController.extend({
+  init() {
+    this._super(...arguments);
+    this.set('sortOptions', [
+      { key: 'createdAt', label: 'Created' },
+      { key: 'updatedAt', label: 'Updated' },
+      { key: 'fullName', label: 'Name' },
+    ]);
+    this.set('sortBy', 'fullName');
+    this.set('ascending', false);
+  },
 });
