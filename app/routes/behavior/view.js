2809cc2509e70931c94e0df2cf2b8c9614550ec7
Jacob Bare <jacob@limit0.io>
2018-06-26 18:41:43 -0500

    Setup view and content query test

--- app/routes/behavior/view.js
+++ app/routes/behavior/view.js
@@ -2,8 +2,19 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import FormMixin from 'leads-manage/mixins/form-mixin';
-import config from 'leads-manage/config/environment';
+
+import query from 'leads-manage/gql/queries/behavior/content-query/view';
 
 export default Route.extend(RouteQueryManager, FormMixin, {
   ohBehaveToken: inject(),
+
+  async model({ id }) {
+    const propertyId = this.get('ohBehaveToken').getPropertyId();
+    const variables = { input: { propertyId, id } };
+
+    const ohBehaveToken = await this.get('ohBehaveToken').retrieve();
+    const context = { ohBehaveToken };
+
+    return this.get('apollo').watchQuery({ query, variables, context, fetchPolicy: 'network-only' }, 'behaviorContentQuery');
+  },
 });
