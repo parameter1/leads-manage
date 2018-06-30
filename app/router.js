46b4f80e72cade2ca2c96a288302b03a530382ef
Jacob Bare <jacob@limit0.io>
2018-06-30 12:56:49 -0500

    Setup initial campaign routes and index

--- app/router.js
+++ app/router.js
@@ -15,6 +15,10 @@ Router.map(function() {
     this.route('edit', { path: ':id' });
     this.route('create');
   });
+  this.route('campaign', function() {
+    this.route('edit', { path: ':id' });
+    this.route('create');
+  });
   this.route('tag', function() {
     this.route('edit', { path: ':id' });
     this.route('create');
