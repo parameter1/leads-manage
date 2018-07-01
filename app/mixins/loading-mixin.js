c08374d42a2d608ce9acd416c000ddc9c9285f98
Jacob Bare <jacob@limit0.io>
2018-07-01 18:46:31 -0500

    Show progess cursor on loading

--- app/mixins/loading-mixin.js
+++ app/mixins/loading-mixin.js
@@ -1,13 +1,16 @@
 import Mixin from '@ember/object/mixin';
 import { inject } from '@ember/service';
+import $ from 'jquery';
 
 export default Mixin.create({
   loadingDisplay: inject(),
 
   showLoading() {
+    $('body').addClass('transitioning');
     this.get('loadingDisplay').show();
   },
   hideLoading() {
     this.get('loadingDisplay').hide();
+    $('body').removeClass('transitioning');
   },
 });
