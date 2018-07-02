48bdc06c4910da6c23c1f68e6af02118bb90ab99
Jacob Bare <jacob@limit0.io>
2018-07-01 22:13:27 -0500

    Handle email campaign excluded fields

--- app/controllers/campaign/edit/email.js
+++ app/controllers/campaign/edit/email.js
@@ -5,6 +5,7 @@ import { computed } from '@ember/object';
 
 import emailCampaignTags from 'leads-manage/gql/mutations/campaign/email/tags';
 import emailCampaignLinkTypes from 'leads-manage/gql/mutations/campaign/email/link-types';
+import emailCampaignExcludedFields from 'leads-manage/gql/mutations/campaign/email/excluded-fields';
 
 export default Controller.extend(FormMixin, {
   apollo: inject(),
@@ -13,7 +14,17 @@ export default Controller.extend(FormMixin, {
 
   linkTypeOptions: computed('linkTypes.types', 'model.allowedLinkTypes', function() {
     const selected = this.get('model.allowedLinkTypes');
-    return this.get('linkTypes.types').filter(o => !selected.includes(o));
+    return this.get('linkTypes.types').filter(type => !selected.includes(type));
+  }),
+
+  excludeFieldOptions: computed('identityAttributes.getViewableFields', 'model.excludeFields', function() {
+    const selected = this.get('model.excludeFields');
+    return this.get('identityAttributes.getViewableFields').filter(o => !selected.includes(o.key));
+  }),
+
+  selectedFieldOptions: computed('identityAttributes.getViewableFields', 'model.excludeFields', function() {
+    const selected = this.get('model.excludeFields');
+    return this.get('identityAttributes.getViewableFields').filter(o => selected.includes(o.key));
   }),
 
   actions: {
@@ -51,6 +62,26 @@ export default Controller.extend(FormMixin, {
       try {
         await this.get('apollo').mutate({ mutation: emailCampaignLinkTypes, variables }, 'emailCampaignLinkTypes');
         this.get('notify').info('Campaign link types set successfully.');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
+    /**
+     *
+     */
+    async updateExcludeFields(fields) {
+      this.startAction();
+      const id = this.get('model.id');
+      const excludeFields = fields.map(field => field.key);
+      const input = { id, excludeFields };
+      const variables = { input };
+
+      try {
+        await this.get('apollo').mutate({ mutation: emailCampaignExcludedFields, variables }, 'emailCampaignExcludedFields');
+        this.get('notify').info('Campaign fields successfully excluded.');
       } catch (e) {
         this.get('graphErrors').show(e);
       } finally {
