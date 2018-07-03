7845afac7a29e62efe0c8f6416fc8678fa8d8fca
Jacob Bare <jacob@limit0.io>
2018-07-03 15:04:56 -0500

    Create components for group campaign links

--- app/controllers/campaign/edit/email/links.js
+++ app/controllers/campaign/edit/email/links.js
@@ -3,4 +3,17 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
   urlGroups: computed.reads('model.urlGroups.[]'),
+
+  actions: {
+    updateExcludedUrls(urlGroup, sendGroups) {
+      const urlId = urlGroup.url.id;
+      const id = this.get('model.id');
+      const excludeUrls = sendGroups.map(sendGroup => ({
+        urlId,
+        sendId: sendGroup.send.id,
+        active: sendGroup.active,
+      }));
+      console.info(id, excludeUrls);
+    },
+  },
 });
