4d76e6b752b635530729548f733dba3bce9c3daa
Jacob Bare <jacob@limit0.io>
2018-07-05 18:55:20 -0500

    Add initial email report metrics query

--- app/routes/lead-report/email/metrics.js
+++ app/routes/lead-report/email/metrics.js
@@ -1,9 +1,17 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import query from 'leads-manage/gql/queries/lead-report/email-metrics';
+
+export default Route.extend(RouteQueryManager, {
+  /**
+   *
+   * @param {object} params
+   */
   model() {
-    return new Promise((resolve) => {
-      setTimeout(resolve, 500);
-    });
+    const hash = this.modelFor('lead-report').get('hash');
+    const variables = { hash };
+
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'reportEmailMetrics');
   },
 });
