ec61e58402d29736fa7032154b543ef33bb95e30
Jacob Bare <jacob@limit0.io>
2018-07-05 12:57:06 -0500

    Set root url to /app

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'leads-manage',
     environment,
-    rootURL: '/manage',
+    rootURL: '/app',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
