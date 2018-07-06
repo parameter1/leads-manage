a4ff127dbb71f1ac4b09dc74f3ac7d0c776c9d60
Jacob Bare <jacob@limit0.io>
2018-07-05 19:36:14 -0500

    Add initial leads table

---
+++ app/components/lead-report/tables/email-metrics.js
@@ -0,0 +1,44 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+
+export default Component.extend({
+  tagName: 'table',
+  classNames: ['table', 'table-striped', 'table-sm', 'table-responsive'],
+
+  title: null,
+
+  init() {
+    this._super(...arguments);
+    this.set('iframe', {
+      show: false,
+      title: null,
+      src: null,
+    });
+  },
+
+  didInsertElement() {
+    // const title = this.get('title');
+    // const table = this.$().DataTable({
+    //   aaSorting: [],
+    //   buttons: [
+    //     { extend: 'excelHtml5', title: title },
+    //     { extend: 'csvHtml5', title: title },
+    //   ],
+    // });
+
+    // $(table.table().container()).addClass('px-0');
+
+    // table.buttons().container()
+    //   .addClass('float-right')
+    //   .appendTo('#' + this.$().attr('id') + '_wrapper .col-md-6:eq(1)')
+    // ;
+  },
+
+  actions: {
+    displayIframeModal(deployment) {
+      this.set('iframe.title', get(deployment, 'name'));
+      this.set('iframe.src', get(deployment, 'url'));
+      this.set('iframe.show', true);
+    },
+  },
+});
