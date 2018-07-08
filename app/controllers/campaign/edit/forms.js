61cbf40baf0e2eba24527eddf02e4b54cd55e301
Jacob Bare <jacob@limit0.io>
2018-07-07 21:47:27 -0500

    Display avaialble forms on campaigns

--- app/controllers/campaign/edit/forms.js
+++ app/controllers/campaign/edit/forms.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import FormMixin from 'leads-manage/mixins/form-mixin';
 import { inject } from '@ember/service';
+import { set } from '@ember/object';
 
 import formCampaignStatus from 'leads-manage/gql/mutations/campaign/forms/status';
+import formCampaignExcludedForms from 'leads-manage/gql/mutations/campaign/forms/excluded-forms';
 
 export default Controller.extend(FormMixin, {
   apollo: inject(),
@@ -22,6 +24,32 @@ export default Controller.extend(FormMixin, {
       try {
         await this.get('apollo').mutate({ mutation: formCampaignStatus, variables }, 'formCampaignStatus');
         this.get('notify').info('Campaign status set successfully.');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
+    async updateExcludedForm(available, event) {
+      this.startAction();
+
+      const { target } = event;
+      const { checked } = target;
+      set(available, 'active', checked);
+
+      const id = this.get('model.id');
+      const excludeForms = this.get('model.forms').map(available => ({
+        formId: available.form.id,
+        active: available.active,
+      }));
+
+      const input = { id, excludeForms };
+      const variables = { input };
+
+      try {
+        await this.get('apollo').mutate({ mutation: formCampaignExcludedForms, variables }, 'formCampaignExcludedForms');
+        this.get('notify').info('Campaign forms successfully set.');
       } catch (e) {
         this.get('graphErrors').show(e);
       } finally {
