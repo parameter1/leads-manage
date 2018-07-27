44dfe8aa411c53ad226de61743f0a2b6ae1327a5
Jacob Bare <jacob@limit0.io>
2018-07-26 19:06:33 -0500

    Add deployment create

--- app/controllers/email/deployment/create.js
+++ app/controllers/email/deployment/create.js
@@ -1,18 +1,38 @@
 import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+import { get } from '@ember/object';
 import FormMixin from 'leads-manage/mixins/form-mixin';
 
+import mutation from 'leads-manage/gql/mutations/email-deployment/create';
+
 export default Controller.extend(FormMixin, {
+  apollo: inject(),
+
   actions: {
     async create() {
       this.startAction();
-      // const payload = { name, description, website };
-      // const variables = { input: { payload } };
-      // return this.get('apollo').mutate({ mutation, variables }, 'createCustomer')
-      //   .then(response => this.transitionTo('customer.edit', response.id))
-      //   .then(() => this.get('notify').info('Customer created successfully.'))
-      //   .catch(e => this.get('graphErrors').show(e))
-      //   .finally(() => this.endRouteAction())
-      // ;
+      const {
+        name,
+        category,
+        subject,
+        preheader,
+      } = this.get('model');
+
+      const input = {
+        name,
+        categoryId: get(category || {}, 'id'),
+        subject,
+        preheader,
+      };
+      const variables = { input };
+      try {
+        const response = await this.get('apollo').mutate({ mutation, variables }, 'createEmailCategory');
+        this.transitionToRoute('email.deployment.index');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
     },
   },
 });
