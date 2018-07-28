4a0dddf16d88210a091df266e69612b4f0e609e5
Jacob Bare <jacob@limit0.io>
2018-07-28 17:07:23 -0500

    Display update+created dates on deployment list

--- app/controllers/email/deployment/index.js
+++ app/controllers/email/deployment/index.js
@@ -4,12 +4,12 @@ export default ListController.extend({
   init() {
     this._super(...arguments);
     this.set('sortOptions', [
-      { key: 'externalSource.createdAt', label: 'Created' },
-      { key: 'externalSource.updatedAt', label: 'Updated' },
-      { key: 'externalSource.lastRetrievedAt', label: 'Last Retrieved' },
+      { key: 'createdAt', label: 'Created' },
+      { key: 'updatedAt', label: 'Updated' },
+      { key: 'lastRetrievedAt', label: 'Last Retrieved' },
       { key: 'name', label: 'Name' },
     ]);
-    this.set('sortBy', 'externalSource.createdAt');
+    this.set('sortBy', 'createdAt');
 
     this.set('searchFields', [
       { key: 'name', label: 'Name' },
