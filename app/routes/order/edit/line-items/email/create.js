913229f91c0ea36d3ea09a5d9fa852277c8961db
Jacob Bare <jacob@limit0.io>
2018-12-09 23:30:02 -0600

    Remove unused services

--- app/routes/order/edit/line-items/email/create.js
+++ app/routes/order/edit/line-items/email/create.js
@@ -1,12 +1,8 @@
 import Route from '@ember/routing/route';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import FormMixin from 'leads-manage/mixins/form-mixin';
-import { inject } from '@ember/service';
 
 export default Route.extend(FormMixin, RouteQueryManager, {
-  identityAttributes: inject(),
-  linkTypes: inject(),
-
   model() {
     return {
       emailCategories: [],
