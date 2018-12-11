2a9064c8844fff64b7af119cc6f35891349840f6
Jacob Bare <jacob@limit0.io>
2018-12-10 20:52:17 -0600

    Add initial email line item index

--- app/routes/order/edit/line-items/email/edit/index.js
+++ app/routes/order/edit/line-items/email/edit/index.js
@@ -4,4 +4,9 @@ export default Route.extend({
   model() {
     return this.modelFor('order.edit.line-items.email.edit');
   },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('order', this.modelFor('order.edit'));
+  },
 });
