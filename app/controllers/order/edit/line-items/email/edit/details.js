9569d7ca9bf78b9d5e99bb551ad39937c8ed9fde
Jacob Bare <jacob@limit0.io>
2018-12-11 18:44:37 -0600

    Add date range mutation

--- app/controllers/order/edit/line-items/email/edit/details.js
+++ app/controllers/order/edit/line-items/email/edit/details.js
@@ -2,14 +2,36 @@ import Controller from '@ember/controller';
 import FormMixin from 'leads-manage/mixins/form-mixin';
 import { inject } from '@ember/service';
 
+import dateRangeMutation from 'leads-manage/gql/mutations/line-item/email/date-range';
+
 export default Controller.extend(FormMixin, {
   apollo: inject(),
-  graphErrors: inject(),
 
   actions: {
-    setDateRange(range) {
-      console.warn('implement setDateRange');
-      this.set('model.range', range);
+    async setDateRange(range) {
+      const { start, end } = range;
+      if (!end) {
+        this.set('model.range', range);
+      } else {
+        this.startAction();
+        const id = this.get('model.id');
+        const input = {
+          id,
+          range: {
+            start: start.valueOf(),
+            end: end.valueOf(),
+          },
+        };
+        const variables = { input };
+        try {
+          await this.get('apollo').mutate({ mutation: dateRangeMutation, variables }, 'emailLineItemDateRange');
+          this.get('notify').info('Date range saved.');
+        } catch (e) {
+          this.get('graphErrors').show(e);
+        } finally {
+          this.endAction();
+        }
+      }
     },
   },
 });
