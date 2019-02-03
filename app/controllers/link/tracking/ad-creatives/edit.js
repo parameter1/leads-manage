bffdaec03aca2d5e2fcb38500e9d9fa006b0af6d
Jacob Bare <jacob@limit0.io>
2019-02-03 09:42:52 -0600

    Add creative tracker edit support

--- app/controllers/link/tracking/ad-creatives/edit.js
+++ app/controllers/link/tracking/ad-creatives/edit.js
@@ -3,28 +3,30 @@ import FormMixin from 'leads-manage/mixins/form-mixin';
 import { get } from '@ember/object';
 import { inject } from '@ember/service';
 
-import mutation from 'leads-manage/gql/mutations/ad-creative-tracker/create';
+import mutation from 'leads-manage/gql/mutations/ad-creative-tracker/update';
 
 export default Controller.extend(FormMixin, {
   apollo: inject(),
 
   actions: {
-    async create() {
+    async update() {
       this.startAction();
-      const { customer, tags, url, description } = this.get('model');
+      const { id, customer, tags, url, description } = this.get('model');
       const payload = {
         customerId: get(customer, 'id'),
         tagIds: tags.map(tag => tag.id),
         url,
         description,
       };
-      const input = { payload };
+      const input = {
+        id,
+        payload,
+      };
       const variables = { input };
       const refetchQueries = ['ListAdCreativeTrackers', 'SearchAdCreativeTrackers'];
 
       try {
-        const response = await this.get('apollo').mutate({ mutation, variables, refetchQueries }, 'createAdCreativeTracker');
-        await this.transitionToRoute('link.tracking.ad-creatives.edit', response.id);
+        await this.get('apollo').mutate({ mutation, variables, refetchQueries }, 'updateAdCreativeTracker');
       } catch (e) {
         this.get('graphErrors').show(e);
       } finally {
