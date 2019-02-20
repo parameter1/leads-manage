d2cdf1703c4beb02ca6dbcc65eeb19a6857b9973
Jacob Bare <jacob@limit0.io>
2019-02-20 07:26:43 -0600

    Display ad campaign tags and add mutation

--- app/controllers/campaign/edit/ads.js
+++ app/controllers/campaign/edit/ads.js
@@ -3,11 +3,33 @@ import FormMixin from 'leads-manage/mixins/form-mixin';
 import { inject } from '@ember/service';
 
 import adCampaignStatus from 'leads-manage/gql/mutations/campaign/ads/status';
+import adCampaignTags from 'leads-manage/gql/mutations/campaign/ads/tags';
 
 export default Controller.extend(FormMixin, {
   apollo: inject(),
 
   actions: {
+    /**
+     *
+     * @param {*} tags
+     */
+    async updateTags(tags) {
+      this.startAction();
+      const id = this.get('model.id');
+      const tagIds = tags.map(tag => tag.id);
+      const input = { id, tagIds };
+      const variables = { input };
+
+      try {
+        await this.get('apollo').mutate({ mutation: adCampaignTags, variables }, 'adCampaignTags');
+        this.get('notify').info('Campaign tags set successfully.');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
     /**
      *
      */
