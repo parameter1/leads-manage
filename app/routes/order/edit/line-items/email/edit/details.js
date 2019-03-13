cecf7a3d8bef66cb6434f785d40c666c246f9e58
Jacob Bare <jacob@limit0.io>
2019-03-13 08:48:37 -0500

    Query on individual email line item edit subroutes

--- app/routes/order/edit/line-items/email/edit/details.js
+++ app/routes/order/edit/line-items/email/edit/details.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
+import { RouteQueryManager } from 'ember-apollo-client';
 
-export default Route.extend({
+import query from 'leads-manage/gql/queries/line-item/email/edit/details';
+
+export default Route.extend(RouteQueryManager, {
   model() {
-    return this.modelFor('order.edit.line-items.email.edit');
+    const id = this.modelFor('order.edit.line-items.email.edit');
+    const variables = { input: { id } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'emailLineItem');
   },
 });
