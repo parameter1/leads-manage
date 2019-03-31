b5248bbe4a202aa45086c63c003c8272cc1d675d
Jacob Bare <jacob@limit0.io>
2019-03-30 22:21:26 -0500

    Use own query params

--- app/controllers/order/edit/line-items/email/edit/leads/index.js
+++ app/controllers/order/edit/line-items/email/edit/leads/index.js
@@ -1,25 +1,51 @@
-import ListController from 'leads-manage/controllers/abstract-list';
+// import ListController from 'leads-manage/controllers/abstract-list';
+import Controller from '@ember/controller';
 import FormMixin from 'leads-manage/mixins/form-mixin';
+// import ListControllerMixin from 'leads-manage/mixins/list-controller-mixin';
 import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 
-export default ListController.extend(FormMixin, {
+export default Controller.extend(FormMixin, {
   identityAttributes: inject(),
   isLoading: computed.or('loadingDisplay.isShowing', 'isActionRunning'),
 
+  queryParams: null,
+
+  id_first: 20,
+
+  id_phrase: '',
+  id_searchType: 'contains',
+  id_searchBy: 'name',
+
+  id_sortBy: null,
+  id_ascending: false,
+
+  isSortDisabled: computed('id_phrase.length', function() {
+    return this.get('id_phrase.length') > 0;
+  }),
+
   init() {
     this._super(...arguments);
+    this.set('queryParams', ['id_first', 'id_sortBy', 'id_ascending', 'id_phrase', 'id_searchType', 'id_searchBy']);
+
     const sortOptions = this.get('identityAttributes.getViewableFields').slice();
 
+    this.set('limitOptions', [10, 20, 50, 100, 200]);
     this.set('sortOptions', [
       { key: 'fieldCount', label: 'Quality' },
       { key: 'createdAt', label: 'Created' },
     ].concat(sortOptions));
-    this.set('first', 50);
-    this.set('sortBy', 'fieldCount');
-    this.set('ascending', false);
+    this.set('id_first', 50);
+    this.set('id_sortBy', 'fieldCount');
+    this.set('id_ascending', false);
 
     this.set('searchFields', this.get('identityAttributes.getViewableFields'));
-    this.set('searchBy', 'emailAddress');
+    this.set('id_searchBy', 'emailAddress');
+  },
+
+  actions: {
+    search(phrase) {
+      this.set('id_phrase', phrase);
+    },
   },
 });
--- app/routes/order/edit/line-items/email/edit/leads/index.js
+++ app/routes/order/edit/line-items/email/edit/leads/index.js
@@ -1,50 +1,45 @@
 import Route from '@ember/routing/route';
-import ListRouteMixin from 'leads-manage/mixins/list-route-mixin';
+import RouteSearchMixin from 'leads-manage/mixins/route-search-mixin';
 
 import query from 'leads-manage/gql/queries/line-item/email/edit/leads/active';
 import search from 'leads-manage/gql/queries/line-item/email/edit/leads/active-search';
 
-export default Route.extend(ListRouteMixin, {
+export default Route.extend(RouteSearchMixin, {
   queryParams: {
-    phrase: {
+    id_phrase: {
       refreshModel: true,
       replace: true,
-      as: 'identityPhrase',
     },
-    searchType: {
+    id_searchType: {
       refreshModel: true,
       replace: true,
-      as: 'identitySearchType',
     },
-    searchBy: {
+    id_searchBy: {
       refreshModel: true,
       replace: true,
-      as: 'identitySearchBy',
     },
-    first: {
+    id_first: {
       refreshModel: true,
-      as: 'identityFirst',
     },
-    sortBy: {
+    id_sortBy: {
       refreshModel: true,
-      as: 'identitySortBy',
     },
-    ascending: {
+    id_ascending: {
       refreshModel: true,
-      as: 'identityAscending',
     },
   },
 
   model({
-    first,
-    sortBy,
-    ascending,
-    phrase,
-    searchType,
-    searchBy,
+    id_first: first,
+    id_sortBy: sortBy,
+    id_ascending: ascending,
+    id_phrase: phrase,
+    id_searchType: searchType,
+    id_searchBy: searchBy,
   }) {
     const id = this.modelFor('order.edit.line-items.email.edit');
     const vars = { input: { id } };
+
     return this.getResults({
       query,
       queryKey: 'emailLineItemActiveIdentities',
@@ -54,5 +49,44 @@ export default Route.extend(ListRouteMixin, {
       searchVars: vars,
       searchKey: 'searchEmailLineItemActiveIdentities',
     }, { first, sortBy, ascending, phrase, searchType, searchBy });
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    const order = this.modelFor('order.edit');
+    controller.set('customerId', order.get('customer.id'));
+  },
+
+  /**
+   *
+   * @param {object} params
+   */
+  async getResults({ query, queryKey, queryVars }, { search, searchKey, searchVars }, { first, sortBy, ascending, phrase, searchType, searchBy }) {
+    const pagination = { first };
+    if (phrase) {
+      return this.search({
+        query: search,
+        resultKey: searchKey,
+        vars: searchVars,
+      }, {
+        searchBy,
+        phrase,
+        searchType,
+        pagination,
+      });
+    }
+
+    const sort = { field: sortBy, order: ascending ? 1 : -1 };
+    const variables = { pagination, sort, ...queryVars };
+    if (!sortBy) delete variables.sort.field;
+
+    this.getController().set('resultKey', queryKey);
+    try {
+      const response = await this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, queryKey);
+      this.getController().set('observable', this.getObservable(response));
+      return response;
+    } catch (e) {
+      this.get('graphErrors').show(e);
+    }
   },
 });
