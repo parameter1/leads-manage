987c696588c397f8e0b89cf72c8543aa124fcb94
Jacob Bare <jacob@limit0.io>
2019-03-31 11:12:12 -0500

    Add loading progress bar and colors

--- app/components/dashboard/data-card.js
+++ app/components/dashboard/data-card.js
@@ -2,8 +2,15 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNames: ['card', 'border-0', 'z-depth-half'],
+  classNames: ['card', 'border-0', 'z-depth-half', 'h-100'],
+  isLoading: false,
   format: '0.00a',
+  color: 'primary',
+
+  textColorClass: computed('color', function() {
+    return `text-${this.get('color')}`;
+  }),
+
   format2: computed('format', 'secondaryFormat', function() {
     const secondaryFormat = this.get('secondaryFormat');
     if (secondaryFormat) return secondaryFormat;
