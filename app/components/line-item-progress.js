29f075095e3e6278abd200dbd333e28b44527823
Jacob Bare <jacob@limit0.io>
2019-03-31 11:12:27 -0500

    Load line item progress after transition

---
+++ app/components/line-item-progress.js
@@ -0,0 +1,33 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+import { ComponentQueryManager } from 'ember-apollo-client';
+import ActionMixin from 'leads-manage/mixins/action-mixin';
+
+import query from 'leads-manage/gql/queries/line-item/email/edit/progress';
+
+export default Component.extend(ComponentQueryManager, ActionMixin, {
+  init() {
+    this._super(...arguments);
+    this.set('qualified', {});
+    this.set('scrubbed', {});
+  },
+
+  didInsertElement() {
+    this.load();
+  },
+
+  async load() {
+    this.startAction();
+    const input = { id: this.get('lineItemId') };
+    const variables = { input };
+    try {
+      const results = await this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'emailLineItem');
+      this.set('qualified', get(results, 'progress.qualified'));
+      this.set('scrubbed', get(results, 'progress.scrubbed'));
+    } catch (e) {
+      this.get('graphErrors').show(e);
+    } finally {
+      this.endAction();
+    }
+  },
+});
