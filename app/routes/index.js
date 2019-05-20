78ee95776ae722d58f7327f3548100d472300042
Jacob Bare <jacob@limit0.io>
2019-05-20 08:37:11 -0500

    Move dashboard to own route; make orders admin only

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,5 @@
 import Route from '@ember/routing/route';
-import ListRouteMixin from 'leads-manage/mixins/list-route-mixin';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-import query from 'leads-manage/gql/queries/line-item/list';
-import search from 'leads-manage/gql/queries/line-item/search';
-
-export default Route.extend(AuthenticatedRouteMixin, ListRouteMixin, {
-  /**
-   *
-   * @param {object} params
-   */
-  model({ first, sortBy, ascending, phrase, searchType, searchBy }) {
-    return this.getResults({
-      query,
-      queryKey: 'allLineItems',
-    }, {
-      search,
-      searchKey: 'searchLineItems',
-    }, { first, sortBy, ascending, phrase, searchType, searchBy });
-  },
+export default Route.extend(AuthenticatedRouteMixin, {
 });
