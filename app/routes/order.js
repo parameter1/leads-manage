0d5131d1fab227d15457c5c6f1c6391c06cacc07
Jacob Bare <jacob@limit0.io>
2019-06-12 08:13:59 -0500

    Give access to Order for Members and higher

--- app/routes/order.js
+++ app/routes/order.js
@@ -2,4 +2,10 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  beforeModel(transition) {
+    if (!this.user.get('isAtLeastMember')) {
+     transition.abort();
+     this.transitionTo('index');
+    }
+  },
 });
