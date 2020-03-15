0f2360b14a125e32e861c730321fbf73d54a78e0
Jacob Bare <jacob@limit0.io>
2020-03-15 09:52:29 -0500

    Setup up initial form line item create

---
+++ app/controllers/order/edit/line-items/form/create.js
@@ -0,0 +1,18 @@
+import Controller from '@ember/controller';
+import FormMixin from 'leads-manage/mixins/form-mixin';
+import { inject } from '@ember/service';
+
+export default Controller.extend(FormMixin, {
+  apollo: inject(),
+  graphErrors: inject(),
+
+  actions: {
+    async create() {
+
+    },
+
+    setDateRange(range) {
+      this.set('model.range', range);
+    },
+  },
+});
