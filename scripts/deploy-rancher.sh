b1b305059bc2056fa059cef713f0da51a8d3b90f
Josh Worden <josh@parameter1.com>
2020-08-19 13:57:28 -0500

    Update build

--- scripts/deploy-rancher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-./scripts/deploy-notify-start.sh
-
-yarn global add @base-cms/travis-rancher-deployer
-deploy-to-rancher "limit0/leads-manage:${TRAVIS_TAG}" leads-manage
-
-./scripts/deploy-notify.sh
