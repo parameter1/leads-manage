8644396845dbefecf21be6ce8df2b128de52e3de
Josh Worden <josh@parameter1.com>
2020-08-19 15:00:52 -0500

    Update build target to P1

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -3,6 +3,6 @@ set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -t "leads-manage:$TRAVIS_TAG" .
-docker tag "leads-manage:$TRAVIS_TAG" "limit0/leads-manage:$TRAVIS_TAG"
-docker push "limit0/leads-manage:$TRAVIS_TAG"
+docker tag "leads-manage:$TRAVIS_TAG" "parameter1/leads-manage:$TRAVIS_TAG"
+docker push "parameter1/leads-manage:$TRAVIS_TAG"
 docker image rm "leads-manage:$TRAVIS_TAG"
