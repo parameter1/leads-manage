15cc461b8a9a5a8c446d979be14574f4695e0778
Jacob Bare <jacob@limit0.io>
2020-09-10 20:24:29 -0500

    Display link assignments on url list items

---
+++ app/components/link/url-list-item.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  mergedTags: computed.union('item.tags', 'item.host.tags'), // eslint-disable-line
+  uniqueTags: computed.uniqBy('mergedTags', 'id'),
+
+  activeCustomer: computed('item.{customer.id,host.customer.id}', function() {
+    console.log(this.get('item.customer.id'), this.get('item.host.customer'));
+    if (this.get('item.customer.id')) return this.get('item.customer');
+    return this.get('item.host.customer');
+  }),
+
+  hasAssignments: computed('assignmentCount', function() {
+    return this.get('assignmentCount') > 0;
+  }),
+
+  assignmentCount: computed('activeCustomer.id', 'mergedTags.length', function() {
+    let count = 0;
+    if (this.get('activeCustomer.id')) count += 1;
+    count += this.get('mergedTags.length');
+    return count;
+  }),
+});
