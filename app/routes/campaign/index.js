07b0d15b0809e1e9bd6129985f6c9e714f1a74d0
Jacob Bare <jacob@limit0.io>
2020-11-02 12:19:52 -0600

    Add customer filter to campaign list page

--- app/routes/campaign/index.js
+++ app/routes/campaign/index.js
@@ -5,17 +5,28 @@ import query from 'leads-manage/gql/queries/campaign/list';
 import search from 'leads-manage/gql/queries/campaign/search';
 
 export default Route.extend(ListRouteMixin, {
+  init() {
+    this._super(...arguments);
+    this.set('queryParams.customers', { refreshModel: true });
+  },
+
   /**
    *
    * @param {object} params
    */
-  model({ first, sortBy, ascending, phrase, searchType, searchBy }) {
+  model({ first, sortBy, ascending, phrase, searchType, searchBy, customers }) {
+    const input = {
+      customerIds: customers.map((customer) => customer.id),
+    };
+
     return this.getResults({
       query,
       queryKey: 'allCampaigns',
+      queryVars: { input },
     }, {
       search,
       searchKey: 'searchCampaigns',
+      searchVars: { input },
     }, { first, sortBy, ascending, phrase, searchType, searchBy });
   },
 });
