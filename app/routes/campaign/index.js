04897209204dcd945b45f39f03814a6cd31ad98a
Jacob Bare <jacob@limit0.io>
2020-11-03 10:24:20 -0600

    Add date filters to campaign list page

--- app/routes/campaign/index.js
+++ app/routes/campaign/index.js
@@ -8,15 +8,43 @@ export default Route.extend(ListRouteMixin, {
   init() {
     this._super(...arguments);
     this.set('queryParams.customers', { refreshModel: true });
+    this.set('queryParams.startingBefore', { refreshModel: true });
+    this.set('queryParams.startingAfter', { refreshModel: true });
+    this.set('queryParams.endingBefore', { refreshModel: true });
+    this.set('queryParams.endingAfter', { refreshModel: true });
   },
 
   /**
    *
    * @param {object} params
    */
-  model({ first, sortBy, ascending, phrase, searchType, searchBy, customers }) {
+  model({
+    first,
+    sortBy,
+    ascending,
+    phrase,
+    searchType,
+    searchBy,
+    customers,
+    startingBefore,
+    startingAfter,
+    endingBefore,
+    endingAfter,
+  }) {
+    const { keys } = Object;
+    const starting = {
+      ...(startingBefore && { before: parseInt(startingBefore, 10) }),
+      ...(startingAfter && { after: parseInt(startingAfter, 10) }),
+    };
+    const ending = {
+      ...(endingBefore && { before: parseInt(endingBefore, 10) }),
+      ...(endingAfter && { after: parseInt(endingAfter, 10) }),
+    };
+
     const input = {
       customerIds: customers.map((customer) => customer.id),
+      ...(keys(starting).length && { starting }),
+      ...(keys(ending).length && { ending }),
     };
 
     return this.getResults({
@@ -28,5 +56,17 @@ export default Route.extend(ListRouteMixin, {
       searchKey: 'searchCampaigns',
       searchVars: { input },
     }, { first, sortBy, ascending, phrase, searchType, searchBy });
+  },
+
+  /**
+   *
+   */
+  actions: {
+    loading(transition) {
+      const controller = this.controllerFor(this.get('routeName'));
+      controller.set('routeLoading', true);
+      transition.promise.finally(() => controller.set('routeLoading', false));
+      return true;
+    },
   },
 });
