810ff1cea7a3b5daefa912e6d618c7534e2e6bf5
Jacob Bare <jacob@parameter1.com>
2021-04-19 16:40:02 -0400

    Update video list page to query from brightcove directly

--- app/routes/video/index.js
+++ app/routes/video/index.js
@@ -1,21 +1,75 @@
 import Route from '@ember/routing/route';
-import ListRouteMixin from 'leads-manage/mixins/list-route-mixin';
+import RouteObservableMixin from 'leads-manage/mixins/route-observable-mixin';
 
 import query from 'leads-manage/gql/queries/video/index';
-import search from 'leads-manage/gql/queries/video/search';
 
-export default Route.extend(ListRouteMixin, {
+export default Route.extend(RouteObservableMixin, {
+  queryParams: {
+    searchPhrase: {
+      refreshModel: true,
+      replace: true,
+    },
+    searchField: {
+      refreshModel: true,
+      replace: true,
+    },
+    limit: {
+      refreshModel: true
+    },
+    sortField: {
+      refreshModel: true
+    },
+    ascending: {
+      refreshModel: true
+    },
+  },
+
   /**
    *
    * @param {object} params
    */
-  model({ first, sortBy, ascending, phrase, searchType, searchBy }) {
+  model({
+    limit,
+    sortField,
+    ascending,
+    searchPhrase,
+    searchField,
+  }) {
     return this.getResults({
       query,
-      queryKey: 'allVideos',
+      queryKey: 'brightcoveCMSVideos',
     }, {
-      search,
-      searchKey: 'searchVideos',
-    }, { first, sortBy, ascending, phrase, searchType, searchBy });
+      limit,
+      sortField,
+      ascending,
+      searchPhrase,
+      searchField,
+    });
+  },
+
+  async getResults({ query, queryKey, queryVars } = {}, {
+    limit,
+    sortField,
+    ascending,
+    searchPhrase,
+    searchField,
+  } = {}) {
+
+    const input = {
+      sort: { field: sortField, order: ascending ? 'ASC' : 'DESC' },
+      ...(limit && { limit }),
+      ...(searchPhrase && { search: { field: searchField, phrase: searchPhrase } }),
+      ...queryVars,
+    };
+
+    const variables = { input };
+    this.getController().set('resultKey', queryKey);
+    try {
+      const response = await this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, queryKey);
+      this.getController().set('observable', this.getObservable(response));
+      return response;
+    } catch (e) {
+      this.get('graphErrors').show(e);
+    }
   },
 });
